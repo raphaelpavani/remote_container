9b273781fa0e5da5873a8d581c145f9926a564b8
Raphael Pavani <raphaelpavani@gmail.com>
2023-08-30 16:55:08 -0300

    atualizou nome tirando underscore

--- projeto_2/desafio_split_join.py
+++ projeto_2/desafio_split_join.py
@@ -3,11 +3,11 @@
 # frase2 = 'jhonatan,rafael,carol,camilla'
 frase1 = 'Desafio manipulação de strings'
 frase2 = 'jhonatan,rafael,carol,camilla'
-palavra_1 = frase1.split()
+palavra1 = frase1.split()
 # Desafio 2: Transforme a frase2 em uma lista de palavras e guarde o resultado em uma variável chamada palavras2
-palavra_2 = frase2.split(',')
+palavra2 = frase2.split(',')
 # Desafio 3: Pegue o palavras1 e transforme elas no seguinte string: "Desafio,manipulação,de,strings". Imprima o resultado no console.
-print(','.join(palavra_1))
+print(','.join(palavra1))
 # Desafio 4: Pegue o palavras2 e transforme elas no seguinte string: frase2 = "jhonatan & rafael & carol & camilla". Imprima o resultado no console.
-frase2 = ' & '.join(palavra_2)
+frase2 = ' & '.join(palavra2)
 print(frase2)
