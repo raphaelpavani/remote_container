c7fdc16a9d6fcb353af4fc1a6504774177c3948c
Raphael Pavani <raphaelpavani@gmail.com>
2023-08-30 15:24:11 -0300

    Adicionados dois argumentos no print

--- projeto_1/variaveis.py
+++ projeto_1/variaveis.py
@@ -2,4 +2,4 @@ variavel_1 = 25.84
 variavel_2 = True
 variavel_3 = 'Bom dia!'
 variavel_4 = 15
-print(type(variavel_1), type(variavel_2),)
+print(type(variavel_1), type(variavel_2), type(variavel_3), type(variavel_4))
