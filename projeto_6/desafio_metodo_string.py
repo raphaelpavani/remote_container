6fb1bd18695f7cf71ee7461564903662a79944aa
Raphael Pavani <raphaelpavani@gmail.com>
2023-08-30 21:24:29 -0300

    Solucao_do desafio_metodo_sting

---
+++ projeto_6/desafio_metodo_string.py
@@ -0,0 +1,19 @@
+# DESAFIO 🥇
+
+# Através da criação de string dinâmicos e os métodos
+# de um string que acabou de aprender, use como base
+# as variáveis a seguir para criar as seguintes frases
+# print('É melhor FEITO que PERFEITO')
+
+a = 'É'
+
+b = 'MELHOR'
+
+c = 'QUE'
+
+d = 'feito'
+
+e = 'perfeito'
+
+
+print(f'{a.upper()} {b.lower()} {d.upper()} {c.lower()} {e.upper()}')
