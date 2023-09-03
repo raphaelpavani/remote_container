ac3877c5287e81d4941919cfa32cfa49e37602e3
Raphael Pavani <raphaelpavani@gmail.com>
2023-09-03 18:48:31 -0300

    Solucao desafio if elif else

---
+++ 3-Aprenda a controlar o fluxo/if_elif_else/desafio_if_elif.py
@@ -0,0 +1,10 @@
+t_cabelo = 51
+
+if t_cabelo <= 20:
+    print("Paga 50 reais.")
+elif 21 <= t_cabelo <= 30:
+    print("Paga 70 reais.")
+elif 31 <= t_cabelo <= 50:
+    print("Paga 100 reais.")
+else:
+    print("Favor consultar na recepção")
