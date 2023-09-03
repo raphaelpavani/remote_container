3006ea94180470e0af944f6f4fec6c2b68999d8a
Raphael Pavani <raphaelpavani@gmail.com>
2023-09-03 20:24:06 -0300

    Solucao desafio while

---
+++ 3-Aprenda a controlar o fluxo/while/desafio_while.py
@@ -0,0 +1,17 @@
+contador = 1
+
+while contador < 121:
+    print(contador)
+    contador += 1
+
+segredo = ""
+
+while segredo != "secreto":
+    segredo = input("Insira a sua senha: ")
+print("Bem-vindo caro usuário!")
+
+
+contador = 100
+while contador > 0:
+    print(contador)
+    contador -= 1
