f4a3f66f2fe7227f52a5b4af803e62635a9ee90f
Raphael Pavani <raphaelpavani@gmail.com>
2023-09-03 19:41:12 -0300

    Solucao desafio nested_loop

---
+++ 3-Aprenda a controlar o fluxo/nested_loops/desafio_nested_loops.py
@@ -0,0 +1,6 @@
+celulares = ['Asus', 'Samsun', 'Sony', 'IPhone']
+versoes = ['Plus', 'Premium Plus', 'Premium Deluxe', 'Plus Premium Ultra']
+
+for celular in celulares:
+    for versao in versoes:
+        print(f'{celular} -> {versao}')
